1d4635c8624fc506ec6e2a1761427190714b835d
Wabi Jayme <wabihaime@gmail.com>
2024-01-21 21:58:43 +0800

    setup materialUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import Providers from "@/providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
---
+++ src/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+const theme = createTheme();
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
